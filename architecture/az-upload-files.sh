30ea19fa3400f55ec0d3eda2e95d9e8ab6d18442
EricJPogue <epogue@epogue.com>
2019-12-02 11:21:01 -0600

    Add and update files.

--- architecture/az-upload-files.sh
+++ architecture/az-upload-files.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-bash az-course-videos.sh --upload-file chapter-18-lecture.pptx &
-bash az-course-videos.sh --upload-file chapter-18-slides.pptx & 
-bash az-course-videos.sh --upload-file chapter-20-slides.pptx &
-bash az-course-videos.sh --upload-file chapter-18-lecture.mp4
+bash az-course-videos.sh --upload-file chapter-20-lecture.pptx &
+bash az-course-videos.sh --upload-file chapter-20-lecture.mp4
