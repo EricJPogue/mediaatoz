2cd506e04971f001b053f9abc66c67fc4003ccfe
EricJPogue <epogue@epogue.com>
2020-08-12 14:33:05 -0500

    Add and remove files.

--- scripts/new-storage-account-mediaatoz.ps1
+++
@@ -1,6 +0,0 @@
-$ResourceGroupName = 'mediaatoz-rg'
-# Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
-$StorageAccountName = 'mediaatoz'
-
-Write-Host "Creating Azure storage account ($StorageAccountName). Please be patient."
-New-AzureRMStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -SkuName "Standard_LRS" -Location "Central US" -Kind "BlobStorage" -AccessTier "hot"
