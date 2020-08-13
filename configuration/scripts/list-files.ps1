a61fc0c9617b5f1773b06b29bc62de72b585279c
EricJPogue <epogue@epogue.com>
2020-08-13 14:22:43 -0500

    Clean up files.

--- configuration/scripts/list-files.ps1
+++
@@ -1,8 +0,0 @@
-$ResourceGroupName = 'mediaatoz-rg'
-$StorageAccountName = 'mediaatoz'
-$ContainerName = 'environment-configuration'
-
-$StorageKey = Get-AzureRMStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
-$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageKey[0].Value
-
-Get-AzureStorageBlob -Container $ContainerName -Context $StorageContext | select Name 
--- object-oriented-programming/scripts/new-storage-account-container-for-object-oriented-programming.ps1
+++
@@ -1,9 +0,0 @@
-$ResourceGroupName = 'mediaatoz-rg'
-$StorageAccountName = 'mediaatoz'
-$ContainerName = 'object-oriented-programming'
-
-$StorageKey = Get-AzureRMStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
-$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageKey[0].Value
-
-Write-Host "Creating Azure storage account container ($ContainerName)."
-New-AzureStorageContainer -Context $StorageContext -Name $ContainerName -Permission Container
